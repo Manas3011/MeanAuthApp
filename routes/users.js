33dff163887463ecca947cc58ffcc6205e3241a0
Manas3011 <manas001.mishra@gmail.com>
2018-10-13 10:49:31 -0700

    Added User Registration System to the Mongo DB.

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const users = require("../models/users");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+
 
 //Register Route
-router.get('/register', (req, res, next)=>{
-    res.send('REGISTER');
+router.post('/register', (req, res, next)=>{
+    let newUser = new users({
+        name: req.body.name,
+        email: req.body.email,
+        userName: req.body.userName,
+        password: req.body.password
+    
+    });
+
+    users.addUser(newUser, (err, user) => {
+        if(err){
+            res.json({success: false, msg: "Failed to Register User"});
+        }else{
+            res.json({success: true, msg: "Registered new User"});
+        }
+    });
 });
 
 //Authenticate Route
-router.get('/authenticate', (req, res, next)=>{
+router.post('/authenticate', (req, res, next)=>{
     res.send('AUTHENTICATE');
 });
 
